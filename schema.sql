81f78f23b96aee1332e7ee1cdfe595dc738b6be1
Кирилл Черепанов <ktcherepanov@nologostudio.ru>
2021-03-06 17:03:29 +0300

    added db schema

---
+++ schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE categories(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE users(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email varchar(255) UNIQUE NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  avatar varchar(50)
+);
+
+CREATE TABLE offers(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title varchar(255) NOT NULL,
+  description text NOT NULL,
+  sum integer NOT NULL,
+  type varchar(5) NOT NULL,
+  picture varchar(50) NOT NULL,
+  created_at timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE comments(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  text text NOT NULL,
+  created_at timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE offer_categories(
+  offer_id integer NOT NULL,
+  category_id integer NOT NULL,
+  PRIMARY KEY (offer_id, category_id),
+  FOREIGN KEY (offer_id) REFERENCES offers(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE INDEX ON offers (title);
